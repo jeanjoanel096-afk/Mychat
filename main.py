206db3c0ce84cfdab51a630819854f7b647a5466
jeanjoanel096-afk <jeanjoanel096@gmail.com>
2026-06-21 22:26:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Baz done senp pou tès
-user_data = {"username": "ProUser", "pwen": 0}
+# Baz done pou estati ak foto
+db = {"status": "Aktif", "photos": []}
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/gade-piblisite', methods=['POST'])
-def gade_piblisite():
-    user_data['pwen'] += 10 # Ajoute 10 pwen
-    return jsonify({"success": True, "nouvo_balans": user_data['pwen']})
+@app.route('/upload-photo', methods=['POST'])
+def upload_photo():
+    if 'photo' in request.files:
+        file = request.files['photo']
+        if file:
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            db['photos'].append(file.filename)
+            return jsonify({"success": True})
+    return jsonify({"success": False})
+
+@app.route('/get-data')
+def get_data():
+    return jsonify(db)
 
 if __name__ == '__main__':
+    if not os.path.exists(UPLOAD_FOLDER): os.makedirs(UPLOAD_FOLDER)
     app.run(host='0.0.0.0', port=10000)
