16466578c82fa93116cec65156b702ac0cb4dbfa
jeanjoanel096-afk <jeanjoanel096@gmail.com>
2026-06-21 23:33:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Sèvi ak PORT Render bay la oswa 10000 pa default
+    # Render itilize pò li bay la, oswa 10000
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
